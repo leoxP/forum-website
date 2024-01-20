1e6e7998b41ff1b318a0c9c9988d60fa78240cd1
leoxP <patrabaibhab@gmail.com>
2024-01-20 20:19:09 +0530

    DB Scheme Updation (added admin + test users)

--- social/forumx/admin.py
+++ social/forumx/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group,User
+from .models import Profile
 
-# Register your models here.
+# Unregister groups
+admin.site.unregister(Group)
+
+# Combine Profile Info into User info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    #Just display username fields on admin page
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+# Unregister initial user    
+admin.site.unregister(User)
+
+# Reregister user and Profile
+admin.site.register(User, UserAdmin)
--- social/forumx/models.py
+++ social/forumx/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create a User Profile Model
+class Profile(models.Model):
+   user = models.OneToOneField(User, on_delete=models.CASCADE) 
+   follows = models.ManyToManyField("self",
+    related_name="followed_by",
+    symmetrical=False, # User does not need to follow back
+    blank=True # User does not need to follow anyone
+    )
+   
+   def __str__(self):
+       return self.user.username
