7b6cc4c0940383c87f8c9f7539ea21d85656865a
leoxP <patrabaibhab@gmail.com>
2024-01-21 23:50:39 +0530

    follow + unfollow added to profiles

--- social/forumx/views.py
+++ social/forumx/views.py
@@ -18,6 +18,22 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+        
+        # Post Form logic
+        if request.method == "POST":
+            # Get current user ID
+            current_user_profile = request.user.profile
+            # Get form data
+            action = request.POST['follow']
+            # Decide to follow or unfollow
+            if action == "unfollow":
+                current_user_profile.follows.remove(profile)
+            elif action == "follow":
+                current_user_profile.follows.add(profile)
+            
+            #Profile saved
+            current_user_profile.save()
+
         return render(request, 'profile.html', {"profile":profile})
     else:
         messages.success(request,("You must be logged in to view profiles"))
